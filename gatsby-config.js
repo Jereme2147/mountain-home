cc0401d981f243435e28113e4f981c0dc2309c67
jereme <jereme.daniels@gmail.com>
2020-12-01 12:25:18 -0500

    still broken

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,21 +45,31 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: `Montserrat`,
-          },
-          {
-            family: `Hind Madurai`,
-          },
-          {
-            family: "Roboto",
-          },
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
-      },
+        display: 'swap'
+      }
     },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Montserrat`,
+    //       },
+    //       {
+    //         family: `Hind Madurai`,
+    //       },
+    //       {
+    //         family: "Roboto",
+    //       },
+    //     ],
+    //   },
+    // },
   ],
 }
 
